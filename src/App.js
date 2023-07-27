51e994299d4f582872531d52bc79e512af1a0be8
phamtu14 <20020493@vnu.edu.vn>
2023-07-27 22:38:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Left from './components/Left';
+import Mid from './components/Mid';
+import Right from './components/Right';
+
+function App() {
+    return (
+        <div className="App">
+            <Left />
+            <Mid />
+            <Right />
+        </div>
+    );
+}
+
+export default App;
